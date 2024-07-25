0985191ba4c71e627529fa9a0ca25c4809185e23
Alex <38862440+amr0ny@users.noreply.github.com>
2024-07-25 17:51:36 +0300

    fv

--- contact_bot/src/bot.ts
+++ contact_bot/src/bot.ts
@@ -56,7 +56,7 @@ export const accessCheckMiddleware = async (ctx: BotContext, next: NextFunction)
     }
 
     const now = new Date();
-    const expirationDate = user.subscription_expiration_date;
+    const expirationDate = user.subscription_expiration_date || new Date(0);
 
     if (expirationDate) {
       if (expirationDate > now) {
