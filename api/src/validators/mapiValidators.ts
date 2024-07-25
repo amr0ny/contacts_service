d1a91a43eaf22fd95f0d2418e0b1654e22f79947
Alex <38862440+amr0ny@users.noreply.github.com>
2024-07-25 03:07:19 +0300

    fff

--- api/src/validators/mapiValidators.ts
+++ api/src/validators/mapiValidators.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
-const InitResponse = Joi.object({  
+const InitResponse = Joi.object({
   Success: Joi.boolean(),
   ErrorCode: Joi.string(),
   TerminalKey: Joi.string().max(20),
@@ -12,22 +12,22 @@ const InitResponse = Joi.object({
 });
 
 const NotificationRequest = Joi.object({
-    TerminalKey: Joi.string(),
-    Amount: Joi.number(),
-    OrderId: Joi.string().max(36),
-    Success: Joi.boolean(),
-    Status: Joi.string().max(20),
-    PaymentId: Joi.string().max(20),
-    ErrorCode: Joi.string().max(20),
-    Message: Joi.string(),
-    Details: Joi.string(),
-    RebillId: Joi.number().max(20),
-    CardId: Joi.number(),
-    Pan: Joi.string(),
-    ExpDate: Joi.string(),
-    Token: Joi.string(),
-    DATA: Joi.object()
-    
+  TerminalKey: Joi.string(),
+  Amount: Joi.number(),
+  OrderId: Joi.string().max(36),
+  Success: Joi.boolean(),
+  Status: Joi.string().max(20),
+  PaymentId: Joi.string().max(20),
+  ErrorCode: Joi.string().max(20),
+  Message: Joi.string(),
+  Details: Joi.string(),
+  RebillId: Joi.number().max(20),
+  CardId: Joi.number(),
+  Pan: Joi.string(),
+  ExpDate: Joi.string(),
+  Token: Joi.string(),
+  DATA: Joi.object()
+
 });
 export const validateInitResponse = (res: any) => InitResponse.validate(res);
 export const validateNotificationRequest = (req: any) => NotificationRequest.validate(req);
